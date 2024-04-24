de892fd5d12ea3b0c8ee2695701369bf84b7c027
tatyana88815 <33359058-tatyana88815@users.noreply.replit.com>
2024-04-24 15:03:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+# print("привет мир")
+# print(1024)
+# print(5)
+# print(10 // 3)
+# print("Добро пожаловать в квиз по стартапам!")
+# print("Ответь на следующие вопросы:")
+# print("Привет \nя использую специальный символ")
+# print(14 // 3)
+# print(14 % 3)
+# print(28)
+ #print(49)
+#print("Никита")
+#print(12)
+#print("принтер,коьпьютер,монитор")
+#print("У лукоморья дуб зелёный;\nЗлатая цепь на дубе том:\nИ днём и ночью кот учёный\nВсё ходит по цепи кругом;")
+#number1 = 10
+#number2 = 5
+#print(number1 + number2)
+#my_name = "Никита"
+#question = "Как называется компания, которая создаётся для развития новой идеи или инновационного продукта?"
+#answer = "Стартап"
+#print(question)
+#print(answer)name = input("Твоё имя: ")
+# здесь получаем имя пользователя
+# name = input("Твоё имя: ")
+# здесь получаем город пользователя
+# city = input("Твой город: ")
+# здесь выводим имя пользователя
+# print(name)
+# здесь выводим город пользователя
+# print(city)
+# question = "Как называется компания, которая создаётся для развития новой идеи или инновационного продукта?"
+# answer = "Стартап"
+# print(question)
+
+# user_input = input("Введите свой ответ: ")
+# if user_input == answer:
+#     print("Правильно!")
+# else:
+#     print("Неправильно!")
+# y = float(input())
+# print(y)
+# text = "Hello"
+# x = 5
+# print(tipe(x))
+# x = int(float(input()))
+# print(x)
+# messege = "Hello\nNikita"
+# print(messege)
+# name = "Никита"
+# print(messege + " " + name)
+#text = "hi"
+# print(text * 3)
+#a = "11"
+# n = "22"
+#print(a + text)
+# projects = ["Сайт для отеля", "Игра-стрелялка", "Калькулятор", "Видеохостинг"]
+# print(projects[2])
+# a = "hello"
+# b = 5
+# c = a * b
+# print(c)
+# a = 7.45
+# b = a * 10
+# print(b)
